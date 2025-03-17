768bac61a8aa0a1202778ba35e89489e32da562f
Micheal <ifymike5@gmail.com>
2025-03-17 04:54:26 -0700

    Fix formatting issues with forge fmt

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.18;
 //import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-
-
 import {PriceConverter} from "./PriceConverter.sol";
 
 error FundMe__NotOwner();
